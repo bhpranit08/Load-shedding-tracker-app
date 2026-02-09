b5a1ce17848cc9b642f0bf322df1c256b7cf1367
Pranit Bhandari <pranitbhandari2019@gmail.com>
2026-02-09 19:40:08 +0545

    Made frontend deployment ready

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': 'http://localhost:5000'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        }
+      }
     }
   }
 })
